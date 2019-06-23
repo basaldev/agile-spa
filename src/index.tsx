6e3ab2ca1d78b4e0faa18c7af7ef99b6a2ff9ef4
Shinsuke Marty Yamada <marty@exodevhub.com>
2019-06-23 16:17:46 +0900

    fix app path

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { subscribe, getState, State } from 'domain/store';
-import { App } from './App';
+import { App } from './app';
 
 function renderer(state: State) {
   ReactDOM.render(<App state={state} />, document.getElementById('root'));
